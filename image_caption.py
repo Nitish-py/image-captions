54b04368f6440d9e20d37f03386c6adc52e48993
Nitish-py <nsqwertfb@gmail.com>
2023-07-10 16:25:44 +0530

    output edited

--- image_caption.py
+++ image_caption.py
@@ -8,7 +8,7 @@ headers = {"Authorization": "Bearer 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"}
 
 def query(data):
     response = requests.post(API_URL, headers=headers, data=data)
-    return response.content
+    return response.json()[0].get('generated_text')
 def main():
     st.set_page_config(page_title="Image Captioning and Title")
     st.header("Image captioning and Title")
@@ -16,17 +16,9 @@ def main():
     image_upload = st.file_uploader("Upload your image", type=["jpeg", "jpg", "png"])
     if image_upload is not None:
         image_data=image_upload.read()
-        #img = Image.open(io.BytesIO(image_data))
         disimg=Image.open(image_upload)
         st.image(disimg)
         response=query(image_data)
-        '''processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-        model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-        inputs = processor(images=input_img, return_tensors="pt")
-        pixel_values = inputs.pixel_values
-        generate_id = model.generate(pixel_values=pixel_values, max_length=50)
-        caption = processor.batch_decode(generate_id, skip_special_tokens=True)[0]
-        '''
         st.write("Caption")
         st.success(response)
         return response
